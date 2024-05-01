0e7d2313a03e6be7714da4c71b7fdbf920e528f1
MathewG2099 <160433914+MathewG2099@users.noreply.github.com>
2024-05-01 19:19:38 -0400

    Packages
    -Added the pacages to the Readme generator

---
+++ index.js
@@ -0,0 +1,23 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');//npmnpm package  require includes modules in the code
+const fs = require('fs') //file system
+const generateMarkdown = require('./take-me-on/utils/generateMarkdown')
+
+
+
+
+
+
+
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
